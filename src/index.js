1eb5ce8d9cfb0692693934ab3e11180c1db57f95
athumma79 <apoorva.thumma79@gmail.com>
2019-06-20 11:34:44 +0100

    routing

--- src/index.js
+++ src/index.js
@@ -3,10 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Login from './login';
+import Home from './home';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const routing = (
+    <Router>
+        <div>
+            <Route path="/home" component={Home} />
+            <Route path="/login" component={Login} />
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'))
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Login />, document.getElementById('root'));
+//ReactDOM.render(<BrowserRouter><Login /></BrowserRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
--- src/login.js
+++ src/login.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import { Form, Button } from 'react-bootstrap'
+import { Form, Button } from 'react-bootstrap';
+import { withRouter } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Home from './home';
 
 class Login extends React.Component {
     render() {
         return (
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                </Form.Group>
+            <div className="text-center" style={{ padding: 20 }}>
+                <h1>Login</h1>
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                    </Form.Group>
 
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
-            </Form>
+                    <Button class="center" variant="primary" type="submit" onClick={this.switchPage}>
+                        Submit
+                    </Button>
+                </Form>
+            </div>
         );
+    }
+    switchPage() {
+        this.history.push('/home');
     }
 }
 
-export default Login;
+export default withRouter(Login);
