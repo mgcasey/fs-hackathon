83ef3c5bd22ffe659ed325563365715eb66b9c6e
athumma79 <apoorva.thumma79@gmail.com>
2019-06-20 11:34:51 +0100

    routing

---
+++ src/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <h1>Properties</h1>
+        );
+    }
+}
+
+export default Home;
